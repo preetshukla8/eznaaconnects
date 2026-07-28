64bd7530393dc9c84fb8911e1122311423518e1f
preetshukla8 <preethsukla8127@gmail.com>
2026-07-28 09:32:43 +0530

    Add Google Analytics tracking

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -91,6 +91,18 @@ function RootShell({ children }: { children: ReactNode }) {
     <html lang="en">
       <head>
         <HeadContent />
+        {/* Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-8JRCZZ7TG5"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-8JRCZZ7TG5');
+            `,
+          }}
+        />
       </head>
       <body>
         {children}
